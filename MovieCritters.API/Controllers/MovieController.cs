bd1bb64f125c836d371deef4a43fe42363f97956
Karina Moreira <kakatinosa@hotmail.com>
2022-10-09 20:29:23 -0300

    Add API project

---
+++ MovieCritters.API/Controllers/MovieController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieCritters.Domain.DTOs;
+using System.Net;
+
+namespace MovieCritters.API.Controllers
+{
+    [Route("[controller]")]
+    public class MovieController : BaseApiController
+    {
+        private readonly ILogger<MovieController> _logger;
+
+        public MovieController(ILogger<MovieController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Fetch a paginated list of movies.
+        /// </summary>
+        /// <param name="pageNumber">Page number starting from 0.</param>
+        /// <param name="pageSize">Page size.</param>
+        /// <returns></returns>
+        [HttpGet]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public IActionResult Get([FromQuery] int pageNumber, int pageSize)
+        {
+            try
+            {
+                return BadRequest(new NotImplementedException());
+            }
+            catch (Exception ex)
+            {
+                var errorMessage = ex.InnerException != null ? ex.InnerException.Message : ex.Message;
+                _logger.LogError(ex, errorMessage);
+                return BadRequest($"Something went wrong when listing movies: {ex.Message}");
+            }
+        }
+
+        /// <summary>
+        /// Fetch a movie.
+        /// </summary>
+        /// <param name="id">Movie Id.</param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(MovieDTO), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            try
+            {
+                return BadRequest(new NotImplementedException());
+            }
+            catch (Exception ex)
+            {
+                var errorMessage = ex.InnerException != null ? ex.InnerException.Message : ex.Message;
+                _logger.LogError(ex, errorMessage);
+                return BadRequest($"Something went wrong when fetching the movie: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ src/services/MovieCritters.Domain/DTOs/MovieDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace MovieCritters.Domain.DTOs
+{
+    public class MovieDTO
+    {
+        public Guid Id { get; set; }
+        public string ImdbId { get; set; }
+        public string Title { get; set; }
+        public string OriginalTitle { get; set; }
+        public string Type { get; set; }
+        public bool IsAdult { get; set; }
+        public DateOnly StartYear { get; set; }
+        public DateOnly EndYear { get; set; }
+        public int RuntimeMinutes { get; set; }
+        public List<string> Genres { get; set; }
+        public double Rating { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
