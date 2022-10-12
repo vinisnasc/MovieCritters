d799eb8461e57887e5aaf972facff6cb5e7f423a
Vinicius Nascimento <83601645+viniciusnasc@users.noreply.github.com>
2022-10-11 23:11:24 -0300

    Create RatingTagHelper

---
+++ src/web/MovieCritters.MVC/Extensions/RatingTagHelper.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace MovieCritters.MVC.Extensions
+{
+    public class RatingTagHelper : TagHelper
+    {
+        public double RateNote { get; set; } 
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "p";
+            var conteudo = "";
+            int num = 0;
+
+            for (int i = 0; i < RateNote; i++)
+            {
+                conteudo += @"<span class = ""fa fa-star checked""></span>";
+                num++;
+            }
+
+            while (num < 5)
+            {
+                conteudo += @"<span class = ""fa fa-star unchecked""></span>";
+                num++;
+            }
+
+            output.Content.SetHtmlContent(conteudo);
+        }
+    }
+}
